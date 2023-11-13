428355640d9b0680aa2ff1e162e1383f78452d34
Ori Hoch <ori@uumpa.com>
2023-11-13 18:19:38 +0200

    documentation fixes

--- tests/cli.py
+++ tests/cli.py
@@ -14,7 +14,8 @@ def main():
 @click.option('--skip-create-cluster', is_flag=True)
 def start_infra(with_observability, build, skip_create_cluster):
     with common.start_infra(with_observability, build, skip_create_cluster):
-        input('Test infra is running, press enter to stop')
+        input('Test infra is running, press enter to stop port forwards')
+    print("Port forwards stopped, but infra is still running, to destroy run: kind delete cluster")
 
 
 if __name__ == '__main__':
